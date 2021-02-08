327c5d1dc06a270ee6af8ccfa53c8df7c510e9c6
Daniel Ramos <dramos@insolpr.com>
2021-02-08 18:55:58 -0400

    commit. Fixed Error.cshtml file problem.

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Test1React.Pages
+namespace WebReaderApp.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
