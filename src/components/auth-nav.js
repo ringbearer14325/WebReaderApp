b166809d26160ade1376006c491483c10cb5278e
Daniel Ramos <danielramos1981@gmail.com>
2021-02-10 14:19:21 -0400

    Commit added auth-nav.js file for adding authetication in a nav bar.

---
+++ src/components/auth-nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AuthenticationButton from "authentication-button.js";
+
+const AuthNav = () => (
+<div className="navbar-nav ml-auto">
+    <AuthenticationButton />
+  </div>
+);
+
+
+
+export default AuthNav;
--- src/components/authentication-button.js
+++ src/components/authentication-button.js
@@ -1,16 +1,17 @@
-import react from "react";
+import React from "react";
 
-import loginButton from "./login-button.js";
-import logoutButton from "./logout-button.js";
+
+import LoginButton from "./login-button";
+import LogoutButton from "./logout-button";
 
 import { useAuth0 } from "@auht/auth0-react";
-import LoginButton from "./login-button.js";
+
 
 const AuthenticationButton = () => {
     const { isAuthenticated } = useAuth0();
     
 
-    return isAuthenticated ? <logoutButton /> : <loginButton />;
+    return isAuthenticated ? <LogoutButton /> : <LoginButton />;
 
 
 };
